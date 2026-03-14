9bdff8f071af421a4d54bc9b798e999405a5ed34
Isharauditha <uditha20916@gmail.com>
2026-03-14 18:59:12 +0530

    update repo name

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/' // ← MUST match your GitHub repo name
+  base: '/Portfolio/' // ← MUST match your GitHub repo name
 })
